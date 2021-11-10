86e70ac77209deef57bd3555b24c3bc0ded208f0
js2docker <44265853+js2docker@users.noreply.github.com>
2021-11-10 13:00:16 -0300

    Revert "changed menu and icon"
    This reverts commit 507a7969a60314828e372594c10e2eb2c013828c.

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -24,18 +24,111 @@ const config: MenuEntry[] = [
   {
     label: 'Farms',
     icon: 'FarmIcon',
-    href: '#comingsoon',
+    href: 'https://bsc.becoswap.com/farms',
   },
   {
     label: 'Pools',
     icon: 'PoolIcon',
-    href: '#comingsoon',
+    href: 'https://bsc.becoswap.com/pools',
   },
   {
-    label: 'Discord',
+    label: 'Referrals',
     icon: 'ReferralIcon',
-    href: 'https://discord.gg/id',
-  }
+    href: 'https://bsc.becoswap.com/referrals',
+  },
+  {
+    label: 'Analytics',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: "Overview",
+        href: 'https://bsc.becoswap.info',
+      },
+      {
+        label: "Tokens",
+        href: 'https://bsc.becoswap.info/tokens',
+      },
+      {
+        label: "Pairs",
+        href: 'https://bsc.becoswap.info/pairs',
+      },
+      {
+        label: "Accounts",
+        href: 'https://bsc.becoswap.info/accounts',
+      },
+    ],
+  },
+  {
+    label: 'Lottery',
+    icon: 'TicketIcon',
+    href: 'https://bsc.becoswap.com/lottery',
+  },
+  {
+    label: 'Launchpad',
+    icon: 'IfoIcon',
+    href: 'https://bsc.becoswap.com/launchpad',
+  },
+  {
+    label: 'NFT Marketplace',
+    icon: 'NftIcon',
+    href: 'https://docs.becoswap.com/products/nft-marketplace',
+  },
+
+  {
+    label: 'Bridge',
+    icon: 'BridgeIcon',
+    href: 'https://docs.becoswap.com/products/cross-chain-bridge',
+  },
+
+  {
+    label: 'Becochain',
+    icon: 'ChainIcon',
+    href: 'https://docs.becoswap.com/products/becochain',
+  },
+
+  {
+    label: 'Stocks',
+    icon: 'StockIcon',
+    href: 'https://docs.becoswap.com/products/stocks',
+  },
+
+  {
+    label: 'Supply Chain',
+    icon: 'SupplyChainIcon',
+    href: 'https://docs.becoswap.com/products/supplychain',
+  },
+  
+  {
+    label: 'Games',
+    icon: 'NftIcon',
+    href: 'https://docs.becoswap.com/products/games',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Contact',
+        href: 'https://docs.becoswap.com/contact-us',
+      },
+      {
+        label: 'Voting',
+        href: 'https://snapshot.org/#/becoswap.eth',
+      },
+      {
+        label: 'Github',
+        href: 'https://github.com/becoswap',
+      },
+      {
+        label: 'Docs',
+        href: 'https://docs.becoswap.com',
+      },
+      {
+        label: 'Blog',
+        href: 'https://becoswap.medium.com',
+      }
+    ],
+  },
 
 ]
 
